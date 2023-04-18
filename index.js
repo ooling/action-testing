7b36810f039f2dcf0ccfb2e3d06117aab209ca6a
Sam oo Líng <samooling@gmail.com>
2023-04-19 00:19:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import('https://unpkg.com/browse/html-minifier-terser@7.2.0/dist/htmlminifier.esm.bundle.js');
 let fs = require('node:fs');
-const { minify } = require('https://unpkg.com/browse/html-minifier-terser@7.2.0/dist/htmlminifier.esm.bundle.js');
+const { minify } = require('html-minifier-terser');
 
 console.log( minify );
 
