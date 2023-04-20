6985694ed11b634a01f30e7eefedce56334a831e
ooling <samooling@gmail.com>
2023-04-21 01:17:34 +0700

    modified:   .github/workflows/manual.yml 	modified:   index.mjs

--- index.mjs
+++ index.mjs
@@ -1,13 +1,2 @@
 
-
-await import('./htdocMinify.mjs').then( async html =>
-
-   await import('node:fs').then( async fs => { try {
-
-      await html.build().then( result => {
-
-         fs.writeFileSync('./targetRepo/index.html', result);
-         console.log('build success')
-      })
-   }  catch (err){ console.log(err) } } )
-)  ;
+console.log(process);
